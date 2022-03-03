3d1452cd96a3bed4ec5bbd6178e689bcabbb05a9
NovusEdge <novusedge0@gmail.com>
2022-03-03 09:42:14 -0800

    Adding the RSADecrypt method

--- rsa.go
+++ rsa.go
@@ -7,16 +7,22 @@ import (
 )
 // RSAEncrypt encrypts plaintext using the RSA (Rivest–Shamir–Adleman)
 // encryption algorithm.
-func RSAEncrypt(plaintext string, keySize int) (ciphertext []byte, publicKey *rsa.PublicKey, privateKey *rsa.PrivateKey, err error) {
+func RSAEncrypt(plaintext []byte, keySize int) (ciphertext []byte, publicKey *rsa.PublicKey, privateKey *rsa.PrivateKey, err error) {
     privateKey, err = GenerateKeyPairs(keySize)
 	if err != nil {
 		return
 	}
 
 	publicKey = &(privateKey.PublicKey)
-	ciphertext, err = rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, []byte(plaintext), nil)
+	ciphertext, err = rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, plaintext, nil)
 
 	return
+}
+
+// RSADecrypt decrypts ciphertext using privateKey and returns the plaintext
+// thus obtained.
+func RSADecrypt(ciphertext []byte, privateKey *rsa.PrivateKey) ([]byte, error) {
+    return privateKey.Decrypt(nil, ciphertext, &rsa.OAEPOptions{Hash: crypto.SHA256})
 }
 
 // GenerateKeyPairs generates private and public keys for RSA encryption.
