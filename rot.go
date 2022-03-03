3df5a871eb4f99d7c14bce99abe2f927e75ee7ae
NovusEdge <novusedge0@gmail.com>
2022-03-03 09:34:19 -0800

    Removing redundant import

--- rot.go
+++ rot.go
@@ -1,41 +1,39 @@
 package crsuite
 
 import (
-    "fmt"
-    "strings"
+	"strings"
 )
-
 
 // ROTEncrypt performs a ROTn encryption on [plaintext] using
 // [offset] and returns it.
 // It is important to note that the ROT encryption performed on
 // plaintext is using right-hand rotation of the text.
 func ROTEncrypt(plaintext string, offset uint) string {
-    rotMapping := func(r rune) rune {
-        switch {
-    	case r >= 'A' && r <= 'Z':
-    		return 'A' + (r-'A'+rune(offset))%26
-    	case r >= 'a' && r <= 'z':
-    		return 'a' + (r-'a'+rune(offset))%26
-    	}
-        return r
-    }
-    return strings.Map(rotMapping, plaintext)
+	rotMapping := func(r rune) rune {
+		switch {
+		case r >= 'A' && r <= 'Z':
+			return 'A' + (r-'A'+rune(offset))%26
+		case r >= 'a' && r <= 'z':
+			return 'a' + (r-'a'+rune(offset))%26
+		}
+		return r
+	}
+	return strings.Map(rotMapping, plaintext)
 }
 
 // ROTDecrypt performs ROTn decryption on plaintext and returns it.
 // The offset specified is taken as a negative value by the function, using
 // which, ROTn operation is performed.
 func ROTDecrypt(ciphertext string, offset uint) string {
-    rotMapping := func(r rune) rune {
-        switch {
-        case r >= 'A' && r <= 'Z':
-            return 'A' + (r-'A'-rune(offset))%26
-        case r >= 'a' && r <= 'z':
-            return 'a' + (r-'a'-rune(offset))%26
-        }
-        return r
-    }
+	rotMapping := func(r rune) rune {
+		switch {
+		case r >= 'A' && r <= 'Z':
+			return 'A' + (r-'A'-rune(offset))%26
+		case r >= 'a' && r <= 'z':
+			return 'a' + (r-'a'-rune(offset))%26
+		}
+		return r
+	}
 
-    return strings.Map(rotMapping, ciphertext)
+	return strings.Map(rotMapping, ciphertext)
 }
